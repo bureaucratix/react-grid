6de2298f9d78d800e022cc20ee5e707fcee40ca0
Bureaucratix <bureaucratix@gmail.com>
2020-04-09 14:07:49 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Container className="App-body">
+        <Row className='App-row'>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+        </Row>
+
+        <Row className='App-row'>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+        </Row>
+
+        <Row className='App-row'>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+        </Row>
+
+        <Row className='App-row'>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+        </Row>
+
+        <Row className='App-row'>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+          <Col className="App-cell"></Col>
+        </Row>
+
+        
+
+      </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
